6e0fdc137b16e5f632c6f2f79d597653a38065f2
Mikdat Dogru <mikdatdogru@gmail.com>
2019-06-14 23:25:12 +0300

    some changes

--- src/index.js
+++ src/index.js
@@ -23,35 +23,37 @@ import './assets/scss/style.scss';
 import configureStore from './utils/configureStore';
 import theme from './utils/theme';
 import App from './containers/App';
-import registerServiceWorker, { unregister } from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
 const { basePath } = window.env;
 export const history = createBrowserHistory({
-    basename: basePath,
+  basename: basePath,
 });
 export const store = configureStore(undefined, history);
 
 const render = Component => {
-    ReactDOM.render(
-        <Provider store={store}>
-            <ThemeProvider theme={theme}>
-                <ConnectedRouter history={history}>
-                    <Component />
-                </ConnectedRouter>
-            </ThemeProvider>
-        </Provider>,
-        document.getElementById('root'),
-    );
+  ReactDOM.render(
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <ConnectedRouter history={history}>
+          <Component />
+        </ConnectedRouter>
+      </ThemeProvider>
+    </Provider>,
+    document.getElementById('root'),
+  );
 };
 
 if (process.env.NODE_ENV === 'development' && module.hot) {
-    // Reload components
-    module.hot.accept('./containers/App', () => {
-        render(App);
-    });
+  // Reload components
+  module.hot.accept('./containers/App', () => {
+    render(App);
+  });
 }
 
 render(App);
 
-// registerServiceWorker();
-unregister();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
